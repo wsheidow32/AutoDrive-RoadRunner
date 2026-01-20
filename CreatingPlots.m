a7be842ea60c2a0e49040fc2addb2671efbf9220
AustinDecot <decot.10@osu.edu>
2026-01-20 15:05:21 -0500

    Deleting Plots Folders
    Deleting Plots Folder and add Threshold Value on Position Error Graph

--- CreatingPlots.m
+++ CreatingPlots.m
@@ -4,6 +4,9 @@ PassFolder = 'Pass';
 FailFolder = 'Fail';
 OtherFolder = "Speed and Map";
 
+% Delete Existing
+rmdir(folderName, 's')
+
 % Creating Folders
 mkdir(folderName, PassFolder)
 mkdir(folderName, FailFolder)
@@ -81,6 +84,7 @@ grid on;
 xlabel('Time (s)');
 ylabel('Position Error (m)');
 title('Position Error vs Time');
+yline(0.3, r, 'Threshold Value');
 
 if any(abs(PositionError.Data) > 0.3)
     PassFail = 'Fail';
