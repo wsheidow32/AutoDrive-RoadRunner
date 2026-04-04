0a614f41c7e489059bdb94c4156bf929bcc63815
djvallen <dylanvall24@gmail.com>
2026-04-04 17:29:09 -0400

    Updated end GUI
    Updated end GUI with added curvature rate plots

--- Trajectory_Smoothness.m
+++ Trajectory_Smoothness.m
@@ -3,6 +3,7 @@ targetActorID = 1;
 longaccelerationFlag = 0;
 lataccelerationFlag = 0;
 curvatureFlag = 0;
+curvaturerateFlag = 0;
 jerkFlag = 0;
 
 if exist('folderName','var') ~= 1 || isempty(folderName)
@@ -167,6 +168,7 @@ if failFlag == 1
     fails(TotalFails).LongitudinalAcceleration = longaccelerationFlag;
     fails(TotalFails).LateralAcceleration = lataccelerationFlag;
     fails(TotalFails).Curvature = curvatureFlag;
+    fails(TotalFails).CurvatureRate = curvaturerateFlag;
     fails(TotalFails).Jerk = jerkFlag;
     fails(TotalFails).LanePosition = laneFlag;
     fails(TotalFails).CollisionDetection = collisionFlag;
